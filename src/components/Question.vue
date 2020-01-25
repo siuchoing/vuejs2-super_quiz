24333639c698c4132458e21963b06a51868b062a
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 15:25:19 +0800

    Implement onAnswer by onClick method

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -8,24 +8,28 @@
                 <button
                         class="btn btn-primary btn-lg"
                         style="margin: 10px"
+                        @click="onAnswer(btnData[0].correct)"
                 >{{ btnData[0].answer }}</button>
             </div>
             <div class="col-xs-12 col-sm-6 text-center">
                 <button
                         class="btn btn-primary btn-lg"
                         style="margin: 10px"
+                        @click="onAnswer(btnData[1].correct)"
                 >{{ btnData[1].answer }}</button>
             </div>
             <div class="col-xs-12 col-sm-6 text-center">
                 <button
                         class="btn btn-primary btn-lg"
                         style="margin: 10px"
+                        @click="onAnswer(btnData[2].correct)"
                 >{{ btnData[2].answer }}</button>
             </div>
             <div class="col-xs-12 col-sm-6 text-center">
                 <button
                         class="btn btn-primary btn-lg"
                         style="margin: 10px"
+                        @click="onAnswer(btnData[3].correct)"
                 >{{ btnData[3].answer }}</button>
             </div>
         </div>
@@ -46,6 +50,10 @@
                     {correct: false, answer: 0}
                 ]
             };
+        },
+        methods: {
+            onAnswer(isCorrect) {
+            }
         }
     }
 </script>
