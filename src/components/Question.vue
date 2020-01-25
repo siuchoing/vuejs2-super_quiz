a8ec732c1237ce24dbcb7b8ecc3c42ef7b54da9b
altantic <15200582@life.hlkbu.edu.hk>
2020-01-25 14:58:06 +0800

    Create Question component

---
+++ src/components/Question.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title text-center">{{ question }}</h3>
+        </div>
+        <div class="panel-body">
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px">{{ btnData[0].answer }}</button>
+            </div>
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px">{{ btnData[1].answer }}</button>
+            </div>
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px">{{ btnData[2].answer }}</button>
+            </div>
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px">{{ btnData[3].answer }}</button>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+        data() {
+            return {
+                question: 'This is a question',
+                btnData: [
+                    {correct: true, answer: 0},
+                    {correct: false, answer: 0},
+                    {correct: false, answer: 0},
+                    {correct: false, answer: 0}
+                ]
+            };
+        }
+    }
+</script>
